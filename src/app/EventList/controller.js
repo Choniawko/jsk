53e41c154d6593f0124d92d9c01964aa035e9e0e
Pawel Choniawko <pawel.choniawko@capgemini.com>
2019-05-31 13:59:52 +0200

    get data events

--- src/app/EventList/controller.js
+++ src/app/EventList/controller.js
@@ -1,12 +1,16 @@
 import { EventListView } from "./view"
 import { store } from "../.."
 
+const events = [
+	{ id: 1, title: "Title #1", description: "Description #1" },
+	{ id: 2, title: "Title #2", description: "Description #2" },
+]
+
 export const EventListController = () => {
-	const { eventList: { events } } = store.getState()
+	store.dispatch({ type: "UPDATE_EVENTS", payload: events })
 	const view = EventListView()
 	const updateList = () => {
-		console.log(events)
-		view.setEvents(events)
+		view.setEvents(store.getState().eventList.events)
 	}
 
 	return {
